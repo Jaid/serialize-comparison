9f658f4c0afec6e270deccedadeb7d8adcd07896
Jaid <jaid.jsx@gmail.com>
2018-05-16 11:13:59 +0200

    Changed sample data

--- src/data.js
+++ src/data.js
@@ -4,17 +4,42 @@ export default {
             valueA: 257,
             valueB: true,
             nested: {
-                valueC: 27
+                valueC: "27"
             }
         },
         string: "TESTSTRING",
-        longString: "This is a veEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEeEery lo0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0ong string!",
+        longString: "This is a veEeEeEeEeEeEeEeEery lo0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0o0ong string!",
         undef: undefined,
         zeroInGerman: null,
         number: 123,
         decimal: 0.1 + 0.3,
         longNumber: 1526322791712,
-        selfmade: [
+        litteNumbers: [
+            5,
+            14,
+            5,
+            9,
+            6,
+            8,
+            9,
+            9,
+            6,
+            8,
+            5,
+            10,
+            11,
+            9,
+            6,
+            5
+        ],
+        objectClone: {
+            valueA: 257,
+            valueB: true,
+            nested: {
+                valueC: "27"
+            }
+        },
+        label: [
             "Felix",
             "Christoph",
             "Jan",
